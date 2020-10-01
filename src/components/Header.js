f80678d799caca3b9f9764f20e518dd9a5281f34
Shreyas Gupta <technology.shreyas@gmail.com>
2020-10-01 14:37:31 +0530

    Improve readability of Header Text

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends React.Component{
                     <Link to="/watchlist">Watchlist</Link>
                 </nav> */}
                 <div className="hero">
-                    <h1 style={{color: "yellow", fontSize: "80px"}}>Find Thy Movie</h1>
+                    <h1 style={{color: "yellow", fontSize: "80px", textShadow: "13px 13px 13px black", fontWeight: "bold"}}>Find Thy Movie</h1>
                     <img src = "https://cdn.hipwallpaper.com/i/78/92/5D4wHq.jpg" style={{objectFit: "cover"}} alt="Movie background"/>
                     {/* <img src="https://i.pinimg.com/originals/a5/11/1d/a5111dd02c562d09778099b673cc47b8.jpg" alt="Movie related background"/> */}
                     {/* <img src="https://images.wallpapersden.com/image/download/star-wars-the-mandalorian_66569_2560x1080.jpg" alt="Movie related background"/> */}
